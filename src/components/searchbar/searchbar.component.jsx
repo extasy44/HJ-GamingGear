1be1401c10d354338552700b91617e6a2862be4b
extasy44 <seoheejun@gmail.com>
2020-06-27 01:26:25 +1000

    handle onSubmit for searchbar

--- src/components/searchbar/searchbar.component.jsx
+++ src/components/searchbar/searchbar.component.jsx
@@ -4,9 +4,13 @@ import "./searchbar.styles.scss";
 import { Search } from "react-bootstrap-icons";
 
 class SearchBar extends React.Component {
+  onSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form className="search-form">
+      <form className="search-form" onSubmit={this.onSubmit.bind(this)}>
         <input
           className="search-input"
           placeholder="SEARCH ENTIRE STORE NOT READY"
